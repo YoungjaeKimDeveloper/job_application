d5753aa33d2428cecbb57522ccbb7a4bfcbac012
Youngjae <2015fromjeki@gmail.com>
2024-07-23 14:19:15 +1000

    Section 3 (4/7)

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,32 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import HomeLayout from "./assets/pages/HomeLayout";
+import {
+  HomeLayout,
+  Landing,
+  Register,
+  Login,
+  DashboardLayout,
+  Error,
+} from "./assets/pages/index.js";
 const router = createBrowserRouter([
   {
     path: "/",
     element: <HomeLayout />,
   },
   {
-    path: "/about",
-    element: (
-      <div>
-        <h1>About Page</h1>
-      </div>
-    ),
+    path: "/register",
+    element: <Register />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/DashboardLayout",
+    element: <DashboardLayout />,
+  },
+  {
+    path: "/Landing",
+    element: <Landing />,
   },
 ]);
 
